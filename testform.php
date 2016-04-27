490bdb95e5dcadf86cfbe09e055b6574a65514e2
Start Coding Student 1 <session1@START-Coding-MBP-09.local>
2016-04-27 14:04:05 -0700

    going through changes

--- testform.php
+++ testform.php
@@ -1,7 +1,74 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: session1
- * Date: 4/27/16
- * Time: 1:45 PM
- */
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Crowned Tailor</title>
+    <link rel="icon" href="images/CT-Logo2.png">
+    <link rel="stylesheet" type="text/css" href="primaryStyle.css">
+</head>
+<body>
+<!--###  body start  ###-->
+<!--# # # # # # # # # #-->
+<!--#   HEADER DIV    #-->
+<!--# # # # # # # # # #-->
+<header>
+    <div id="head">
+        <div id="siteName">
+            <a href="index.php"><img src="images/CT-LogoMain.png"></a>
+        </div>
+        <br>
+        <div id="signIn">
+            <?php
+            if($_COOKIE['logUser'] != null){
+                ?>
+                <ul>
+                    <li><a href="logout.php">Log-Out</a></li>
+                    <li><a href="admin.php"><?php echo $_COOKIE['logUser']?></a></li>
+                </ul>
+                <?php
+            }else{
+                ?>
+                <ul>
+                    <li><a href="account.php">Sign In</a></li>
+                </ul>
+                <?php
+            }
+            ?>
+        </div>
+    </div>
+    <br><br><br>
+    <div id="navbar">
+        <ul>
+            <li><a href="about.php">About</a></li>
+            <li><a href="index.php">Home</a></li>
+        </ul>
+    </div>
+</header>
+<!--***  header end  ***-->
+<!--# # # # # # # # # # -->
+<!--#   CONTENT DIV   #-->
+<!--# # # # # # # # # # -->
+<div id="content">
+    <img style="width: 40%" src="https://res.cloudinary.com/teepublic/image/private/s--XPaPGZnc--/t_Preview/b_rgb:ffffff,c_limit,f_jpg,h_630,q_90,w_630/v1454323348/production/designs/409266_1">
+</div>
+<!--***  content end  ***-->
+<!--# # # # # # # # # #-->
+<!--#   FOOTER DIV    #-->
+<!--# # # # # # # # # #-->
+<footer>
+    <div id="copyright">
+        <p>&copy 2016 - Crowned Tailor Clothing Co. All Rights Reserved</p>
+    </div>
+    <div id="lowLinks">
+        <a href="index.php">Home</a>
+        <a href="account.php">Sign-In</a>
+        <a href="about.php">About Us</a>
+        <img src="images/CT-Logo2.png">
+    </div>
+</footer>
+<!--***  footer end   ***-->
+<!--***    body end   ***-->
+</body>
+</html>
