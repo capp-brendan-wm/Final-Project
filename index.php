2922c3a7ae4238ae284de22d5c1c5a7310493263
Start Coding Student 1 <session1@START-Coding-MBP-09.local>
2016-05-03 14:01:09 -0700

    index prt1

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ error_reporting(0); // disables all error messages.
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Crowned Tailor</title>
+    <title>Crowned Tailor - Index</title>
     <link rel="icon" href="images/CT-icon.png">
-    <link rel="stylesheet" type="text/css" href="primaryStyle.css">
+    <link rel="stylesheet" type="text/css" href="indexStyle.css">
 </head>
 <body>
 <!--###  body start  ###-->
@@ -67,7 +67,58 @@ error_reporting(0); // disables all error messages.
 <!--#   CONTENT DIV   #-->
 <!--# # # # # # # # # # -->
 <div id="content">
-    <img style="width: 40%" src="https://s-media-cache-ak0.pinimg.com/736x/a1/99/c6/a199c69ed7f6b52943df06c06d092e29.jpg">
+    <!-- Start cssSlider -->
+    <div class='csslider1 autoplay '>
+        <input name="cs_anchor1" id='cs_slide1_0' type="radio" class='cs_anchor slide' >
+        <input name="cs_anchor1" id='cs_slide1_1' type="radio" class='cs_anchor slide' >
+        <input name="cs_anchor1" id='cs_slide1_2' type="radio" class='cs_anchor slide' >
+        <input name="cs_anchor1" id='cs_play1' type="radio" class='cs_anchor' checked>
+        <input name="cs_anchor1" id='cs_pause1' type="radio" class='cs_anchor' >
+        <ul>
+            <div style="width: 100%; visibility: hidden; font-size: 0px; line-height: 0;">
+                <img src="https://newevolutiondesigns.com/images/freebies/city-wallpaper-5.jpg" style="width: 100%;">
+            </div>
+            <li class='num0 img'>
+                <a href="http://cssslider.com" target=""><img src='http://home.bt.com/images/queen-elizabeth-ii-coronation-crown-137477997009502601-130726102131.jpg' alt='slide1' title='slide1' /> </a>
+            </li>
+            <li class='num1 img'>
+                <a href="http://cssslider.com" target=""><img src='http://static1.squarespace.com/static/561f9bcae4b0f197c395011c/t/561fcf31e4b0ad1f9959cf43/1444925234702/diytailoring.jpg?format=1500w' alt='slide2' title='slide2' /> </a>
+            </li>
+            <li class='num2 img'>
+                <a href="http://cssslider.com" target=""><img src='http://edc.h-cdn.co/assets/cm/15/04/54c19810eab0d_-_zoey-deschanel-tommy-fashion-line-95399786.jpg' alt='slide3' title='slide3' /> </a>
+            </li>
+
+        </ul>
+        <div class='cs_description'>
+            <label class='num0'>
+                <span class="cs_title"><span class="cs_wrapper">We are CROWNED</span></span>
+            </label>
+            <label class='num1'>
+                <span class="cs_title"><span class="cs_wrapper">Get Your Clothing TAILORED</span></span>
+            </label>
+            <label class='num2'>
+                <span class="cs_title"><span class="cs_wrapper">Take our Clothing Quiz!</span></span>
+            </label>
+        </div>
+
+        <div class='cs_bullets'>
+            <label class='num0' for='cs_slide1_0'>
+                <span class='cs_point'></span>
+                <span class='cs_thumb'><img src='http://home.bt.com/images/queen-elizabeth-ii-coronation-crown-137477997009502601-130726102131.jpg' alt='slide1' title='slide1' /></span>
+            </label>
+            <label class='num1' for='cs_slide1_1'>
+                <span class='cs_point'></span>
+                <span class='cs_thumb'><img src='http://static1.squarespace.com/static/561f9bcae4b0f197c395011c/t/561fcf31e4b0ad1f9959cf43/1444925234702/diytailoring.jpg?format=1500w' alt='slide2' title='slide2' /></span>
+            </label>
+            <label class='num2' for='cs_slide1_2'>
+                <span class='cs_point'></span>
+                <span class='cs_thumb'><img src='http://edc.h-cdn.co/assets/cm/15/04/54c19810eab0d_-_zoey-deschanel-tommy-fashion-line-95399786.jpg' alt='slide3' title='slide3' /></span>
+            </label>
+        </div>
+    </div>
+    <!-- End cssSlider -->
+    <img style="width: 50%; float: Left" src="images/crown1.jpg">
+
 </div>
 <!--***  content end  ***-->
 <!--# # # # # # # # # #-->
