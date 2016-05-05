2f49f6e4349649a1d60d7e8055c9d84942201fc7
Zachary Schweikert <zachary.schweikert@west-mec.com>
2016-05-05 15:27:23 -0700

    This now displays the questions from the databases.

--- testform.php
+++ testform.php
@@ -27,7 +27,7 @@ session_start();
                 $query = "SELECT * FROM users WHERE username = :username1";
                 $stmt = $dbh->prepare($query);
                 $stmt->execute(array(
-                    'username1' => $_SESSION['username1']
+                    'username1' => $_COOKIE['logUser']
                 ));
                 $result= $stmt->fetchAll();
 
@@ -65,148 +65,77 @@ session_start();
 <!--# # # # # # # # # # -->
 <!--#   CONTENT DIV   #-->
 <!--# # # # # # # # # # -->
-<div id="content">
-    <?php
-    if($_COOKIE['logUser'] != null){
-        $dbh = new PDO('mysql:host=localhost;dbname=ct.db', 'root', 'root');
+<div id="content" style="text-align: left">
+<?php
 
-        $query = "SELECT * FROM users WHERE username = :username1";
-        $stmt = $dbh->prepare($query);
-        $stmt->execute(array(
-            'username1' => $_SESSION['username1']
-        ));
-        $result= $stmt->fetchAll();
-        ?>
-        <h1><?php echo $_COOKIE['logUser']; ?>, Welcome to our Crowned Clothing Personality Quiz!</h1>
-        <!--##############################################################################################################-->
-        <!--##############################################################################################################-->
-        <!--##############################################################################################################-->
-        <!--##############################################################################################################-->
-        <!--##############################################################################################################-->
+if ($_POST['submit'] == null) {
 
-        <?php
-        foreach($result as $row){
-            $thyID = $row['user_id'];
-            $query = "SELECT * FROM ct_response WHERE users_user_id = :users_user_id";
-            $stmt = $dbh->prepare($query);
-            $stmt->execute(array(
-                'users_user_id' => $thyID
-            ));
-            $data= $stmt->fetchAll();
+    $dbh = new PDO('mysql:host=localhost;dbname=ct.db', 'root', 'root');
 
-            require_once('appvars.php');
+    $query = "SELECT * FROM ct_questions";
+    $stmt = $dbh->prepare($query);
+    $stmt->execute(array(
+    //'username1' => $_COOKIE['logUser']
+    ));
+    $result= $stmt->fetchAll();
 
-            if (count($data) == 0) {
-                $topicIDs = array();
 
-                $query2 = "SELECT topic_id FROM ct_topic ORDER BY category, ct_type_type_id";
-                $stmt2 = $dbh->prepare($query2);
-                $stmt2->execute();
-                $data2 = $stmt->fetchAll();
+    echo "<form method='post'><table>";
+            foreach ($result as $row) {
+            echo "<tr> <td>" .
+                    $row['question'] . "<br>" .
+                    "  </td>";
 
-                foreach ($data2 as $row2) {
-                    array_push($topicIDs, $row2['topic_id']);
-                }
 
-                // Insert empty response rows into the response table, one per topic
-                foreach ($topicIDs as $topic_id) {
-                    //$query = "INSERT INTO mismatch_response (user_id, topic_id) VALUES ('" . $_SESSION['user_id']. "', '$topic_id')";
-                    //mysqli_query($dbc, $query);
+                if ($row['ct_type_type_id'] > 0) {
 
-                    $query = "INSERT INTO ct_response (users_user_id, topic_id) VALUES (:users_user_id, :topic_id)";
-                    $stmt = $dbh->prepare($query);
-                    $stmt->execute(array(
-                        'users_user_id' => $thyID,
-                        'topic_id' => $topic_id
-                    ));
-                }
-            }
 
-        }
+                $dbh2 = new PDO('mysql:host=localhost;dbname=ct.db', 'root', 'root');
 
-// If the questionnaire form has been submitted, write the form responses to the database
-        if (isset($_POST['submit'])) {
-            // Write the questionnaire response rows to the response table
-            foreach ($_POST as $response_id => $response) {
-                //$query = "UPDATE mismatch_response SET response = '$response' WHERE response_id = '$response_id'";
-                //mysqli_query($dbc, $query);
-
-                $query = "UPDATE ct_response SET response = :response WHERE response_id = :response_id";
-                $stmt = $dbh->prepare($query);
-                $stmt->execute(array(
-                    'response' => $response,
-                    'response_id' => $response_id
+                $query2 = "SELECT * FROM ct_topic WHERE ct_questions_question_id = :ct_questions_question_id";
+                $stmt2 = $dbh2->prepare($query2);
+                $stmt2->execute(array(
+                'ct_questions_question_id' => $row['ct_type_type_id']
                 ));
+                $result2= $stmt2->fetchAll();
+
+                echo "<td>";
+                    foreach ($result2 as $row2) {
+
+                    echo "<input type='checkbox' value='1' name='" . $row2['ct_type_type_id'] . "'>";
+                    echo $row2['topic'] . "<br>";
+
+                    }
+                    echo "</td>";
 
 
+                }
 
+
+                ;
+                echo "</tr>";
             }
-            echo '<p>Your responses have been saved.</p>';
-        }
-
-// Grab the response data from the database to generate the form
-//$query = "SELECT mr.response_id, mr.topic_id, mr.response, mt.name AS topic_name, mc.name AS category_name " .
-//  "FROM mismatch_response AS mr " .
-//  "INNER JOIN mismatch_topic AS mt USING (topic_id) " .
-//  "INNER JOIN mismatch_category AS mc USING (category_id) " .
-//  "WHERE mr.user_id = '" . $_SESSION['user_id'] . "'";
+            echo "<tr><td><input type='submit' value='Submit the form!' name='submit'></td></tr></table></form>";
 
 
-        $query = "SELECT mr.response_id, mr.topic_id, mr.response, mt.name AS topic_name, mc.name AS category_name " .
-            "FROM mismatch_response AS mr " .
-            "INNER JOIN mismatch_topic AS mt USING (topic_id) " .
-            "INNER JOIN mismatch_category AS mc USING (category_id) " .
-            "WHERE mr.user_id = :user_id";
-        $stmt = $dbh->prepare($query);
-        $stmt->execute(array(
-            'user_id' => $_SESSION['user_id']
-        ));
-        $data = $stmt->fetchAll();
-
-//$data = mysqli_query($dbc, $query);
-        $responses = array();
-        foreach ($data as $row) {
-            array_push($responses, $row);
-        }
-
-//mysqli_close($dbc);
-
-// Generate the questionnaire form by looping through the response array
-        echo '<form method="post" action="' . $_SERVER['PHP_SELF'] . '">';
-        echo '<p>Please Answer the Following Questions</p>';
-        $category = $responses[0]['category_name'];
-        echo '<fieldset><legend>' . $responses[0]['category_name'] . '</legend>';
-        foreach ($responses as $response) {
-            // Only start a new fieldset if the category has changed
-            if ($category != $response['category_name']) {
-                $category = $response['category_name'];
-                echo '</fieldset><fieldset><legend>' . $response['category_name'] . '</legend>';
-            }
-
-            // Display the topic form field
-            echo '<label ' . ($response['response'] == NULL ? 'class="error"' : '') . ' for="' . $response['response_id'] . '">' . $response['topic_name'] . ':</label>';
-            echo '<input type="radio" id="' . $response['response_id'] . '" name="' . $response['response_id'] . '" value="1" ' . ($response['response'] == 1 ? 'checked="checked"' : '') . ' />Love ';
-            echo '<input type="radio" id="' . $response['response_id'] . '" name="' . $response['response_id'] . '" value="2" ' . ($response['response'] == 2 ? 'checked="checked"' : '') . ' />Hate<br />';
-        }
-        echo '</fieldset>';
-        echo '<input type="submit" value="Save Questionnaire" name="submit" />';
-        echo '</form>';
-        ?>
+}
+else {
+    echo "you took the test";
 
 
-        <!--##############################################################################################################-->
-        <!--##############################################################################################################-->
-        <!--##############################################################################################################-->
-        <!--##############################################################################################################-->
-        <!--##############################################################################################################-->
-        <?php
-    }else{
-        ?>
-        <h1>You have found our Crowned Clothing Personality Quiz</h1>
-        <h2>We've noticed you are not logged-in, to access the quiz and save your results, please <a href="account.php">Sign In</a> </h2>
-        <?php
-    }
+
+
+
+}
+
     ?>
+
+    <style>
+        td {
+            border: 1px;
+            border-style: double;
+        }
+    </style>
 </div>
 <!--***  content end  ***-->
 <!--# # # # # # # # # #-->
