d5aafe2e2bdff74dfdf0c6a9f78ca3c8c2092c6b
Brendan Capp <brendan.capp@west-mec.org>
2016-05-05 13:28:56 -0700

    Creation of tables for database.

---
+++ ct_response.sql
@@ -0,0 +1,6 @@
+/*
+-- Query: SELECT * FROM `ct.db`.ct_response
+LIMIT 0, 1000
+
+-- Date: 2016-05-05 13:26
+*/
---
+++ ct_topic.sql
@@ -0,0 +1,66 @@
+/*
+-- Query: SELECT * FROM `ct.db`.ct_topic
+LIMIT 0, 1000
+
+-- Date: 2016-05-05 13:26
+*/
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (1,'Going out to the beach and hanging with a fri','A',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (2,'Dogs','A',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (3,'White Chicks','A',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (4,'Spongebob','A',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (5,'Fiji','A',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (6,'Pop','A',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (7,'Sunny','A',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (8,'Playing Sports','A',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (9,'Cookies and Cream','A',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (10,'Physical Education','A',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (11,'Instagram','A',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (12,'Football','A',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (13,'Hats','A',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (14,'The Suite Life of Zach and Cody','A',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (15,'Suh Dude','A',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (16,'Exploring museums and thrifting at all the lo','B',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (17,'Snakes','B',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (18,'The Grudge','B',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (19,'Sailor Moon','B',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (20,'Tokyo, Japan','B',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (21,'House/Electric','B',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (22,'Rainy','B',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (23,'Painting','B',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (24,'Mint Chocolate Chip','B',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (25,'Art','B',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (26,'Tumblr','B',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (27,'Gymnastics','B',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (28,'Accessories','B',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (29,'The Grim Adventures of Billy and Mandy','B',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (30,'Hey','B',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (31,'Visiting the amusement park.','C',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (32,'Hamsters','C',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (33,'Fast and Furious','C',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (34,'Chowder','C',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (35,'Santorini, Greece','C',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (36,'Rock','C',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (37,'Windy','C',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (38,'Skateboarding','C',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (39,'Strawberry Cheesecake','C',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (40,'Theatre','C',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (41,'Twitter','C',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (42,'Baseball','C',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (43,'Clothes','C',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (44,'ICarly','C',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (45,'Hola','C',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (46,'Chilling at home curled up with a good book.','D',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (47,'Cats','D',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (48,'Lion King','D',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (49,'Bob\'s Burgers','D',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (50,'Sedona, U.S.A.','D',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (51,'Indie','D',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (52,'Cloudy','D',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (53,'Cooking','D',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (54,'Vanilla','D',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (55,'Literature','D',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (56,'Pinterest','D',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (57,'Track','D',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (58,'Shoes','D',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (59,'Pokemon','D',0,0);
+INSERT INTO `ct_topic` (`topic_id`,`topic`,`category`,`ct_type_type_id`,`ct_questions_question_id`) VALUES (60,'What\'s Up','D',0,0);
