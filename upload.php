fbf2d63810c3d5851875048e697df7c0d0bec3c2
Zachary Schweikert <zachary.schweikert@west-mec.com>
2016-05-09 15:01:17 -0700

    Finished with the Upload page unless links to other pages or something wish to be added.

--- upload.php
+++ upload.php
@@ -20,27 +20,28 @@ error_reporting(0); // disables all error messages.
             <a href="index.php"><img src="images/CT-LogoMain.png"></a>
         </div>
         <br>
-        <?php
-        if($_COOKIE['logUser'] != null){
-            $dbh = new PDO('mysql:host=localhost;dbname=ct.db', 'root', 'root');
+        <div id="signIn">
+            <?php
+            if($_COOKIE['logUser'] != null){
+                $dbh = new PDO('mysql:host=localhost;dbname=ct.db', 'root', 'root');
 
-            $query = "SELECT * FROM users WHERE username = :username1";
-            $stmt = $dbh->prepare($query);
-            $stmt->execute(array(
-                'username1' => $_SESSION['username1']
-            ));
-            $result= $stmt->fetchAll();
+                $query = "SELECT * FROM users WHERE username = :username1";
+                $stmt = $dbh->prepare($query);
+                $stmt->execute(array(
+                    'username1' => $_COOKIE['logUser']
+                ));
+                $result= $stmt->fetchAll();
 
-            foreach($result as $row) {
-                $username1 = $row['username'];
-                $image = $row['prof_image']; // use this as a profile photo so there's something to upload.
-            }
-            ?>
-            <ul>
-                <li><a href="logout.php">Log-Out</a></li>
-                <li><a href="account.php"><img src="images/<?php echo $image; ?>" id="image"></a></li>
-                <li><a href="admin.php"><?php echo $_COOKIE['logUser']?></a></li>
-            </ul>
+                foreach($result as $row) {
+                    $username1 = $row['username'];
+                    $image = $row['prof_image']; // use this as a profile photo so there's something to upload.
+                }
+                ?>
+                <ul>
+                    <li><a href="logout.php">Log-Out</a></li>
+                    <li><a href="account.php"><img src="images/<?php echo $image; ?>" id="image"></a></li>
+                    <li><a href="admin.php"><?php echo $_COOKIE['logUser']?></a></li>
+                </ul>
                 <?php
             }else{
                 ?>
@@ -65,8 +66,117 @@ error_reporting(0); // disables all error messages.
 <!--# # # # # # # # # # -->
 <!--#   CONTENT DIV   #-->
 <!--# # # # # # # # # # -->
+<!--***  header end  ***-->
+<!--# # # # # # # # # # -->
+<!--#   CONTENT DIV   #-->
+<!--# # # # # # # # # # -->
 <div id="content">
-    <img style="width: 40%" src="https://s-media-cache-ak0.pinimg.com/736x/a1/99/c6/a199c69ed7f6b52943df06c06d092e29.jpg">
+
+
+<?php
+
+    if ($_SESSION['loggedIn'] == 1) {
+
+    define('MM_UPLOADPATH', 'images/');
+    define('MM_MAXFILESIZE', 10000000);      //  KB
+    define('MM_MAXIMGWIDTH', 1200);        //  pixels
+    define('MM_MAXIMGHEIGHT', 1200);       //  pixels
+
+    if (isset($_POST['submit'])) {
+    // Grab the profile data from the POST
+    $old_picture = trim($_POST['old_picture']);
+    $new_picture = trim($_FILES['screenshot']['name']);
+    $new_picture_type = $_FILES['screenshot']['type'];
+    $new_picture_size = $_FILES['screenshot']['size'];
+    list($new_picture_width, $new_picture_height) = getimagesize($_FILES['screenshot']['tmp_name']);
+    $error = false;
+
+    // Validate and move the uploaded picture file, if necessary
+    if (!empty($new_picture)) {
+    if ((($new_picture_type == 'image/gif') || ($new_picture_type == 'image/jpeg') || ($new_picture_type == 'image/pjpeg') ||
+    ($new_picture_type == 'image/png')) && ($new_picture_size > 0) && ($new_picture_size <= MM_MAXFILESIZE) &&
+    ($new_picture_width <= MM_MAXIMGWIDTH) && ($new_picture_height <= MM_MAXIMGHEIGHT)) {
+    if ($_FILES['file']['error'] == 0) {
+    // Move the file to the target upload folder
+    $target = MM_UPLOADPATH . basename($new_picture);
+    if (move_uploaded_file($_FILES['screenshot']['tmp_name'], $target)) {
+    // The new picture file move was successful, now make sure any old picture is deleted
+    if (!empty($old_picture) && ($old_picture != $new_picture)) {
+    @unlink(MM_UPLOADPATH . $old_picture);
+    }
+    }
+    else {
+    // The new picture file move failed, so delete the temporary file and set the error flag
+    @unlink($_FILES['screenshot']['tmp_name']);
+    $error = true;
+    // echo '<p class="error">Sorry, there was a problem uploading your picture.</p>';
+    }
+    }
+    }
+    else {
+    // The new picture file is not valid, so delete the temporary file and set the error flag
+    @unlink($_FILES['screenshot']['tmp_name']);
+    $error = true;
+    echo '<p class="error">Your picture must be a GIF, JPEG, or PNG image file no greater than ' . (MM_MAXFILESIZE / 1024) .
+        ' KB and ' . MM_MAXIMGWIDTH . 'x' . MM_MAXIMGHEIGHT . ' pixels in size.</p>';
+    }
+    }
+
+
+    // Update the profile data in the database
+    if (!$error) {
+
+    // Only set the picture column if there is a new picture
+
+    $dbh = new PDO('mysql:host=localhost;dbname=ct.db', 'root', 'root');
+
+    $query = "INSERT INTO ct_uploads VALUES (0, :screenshot, :username1)";
+    $stmt = $dbh->prepare($query);
+    $stmt->execute(array(
+    'username1' => $_COOKIE['logUser'],
+    'screenshot' => $new_picture
+    ));
+    // Confirm success with the user
+    // echo '<p>Your profile has been successfully updated. Would you like to <a href="viewprofile.php">view your profile</a>?</p>';
+    //exit();
+    }
+    $_POST['submit'] = null;
+    // header('location: account.php?fixit=true');
+        echo "<h1> Your Clothing has been successfully uploaded! </h1>";
+    } // End of check for form submission
+
+    else {
+        ?>
+<br>
+        <div id="main">
+
+            Share your clothing with the community!
+            <form enctype="multipart/form-data" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+
+                <input type="hidden" name="old_picture" value="<?php if (!empty($old_picture)) echo $old_picture; ?>" />
+
+                Photo to Submit
+                <input type="hidden" name="MAX_FILE_SIZE" value="10000000" />
+                <input type="file" id="screenshot" name="screenshot"/>
+                <br>
+                <input type="submit" value="Share these clothes!" name="submit" />
+            </form>
+
+        </div>
+        <br>
+        <?php
+
+    }
+
+
+
+}
+else {
+    echo "<h1> You must be logged in to upload images </h1>";
+}
+?>
+
+
 </div>
 <!--***  content end  ***-->
 <!--# # # # # # # # # #-->
